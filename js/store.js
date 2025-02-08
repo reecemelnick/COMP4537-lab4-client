269bb221a1c420ce85efd248cef6d202a5e9707a
Reece Melnick <reececianmelnick@gmail.com>
2025-02-08 13:53:59 -0800

    client

---
+++ js/store.js
@@ -0,0 +1,84 @@
+function post(wordInput, wordDefinition) {
+
+    document.getElementById('valid-word').innerHTML = "";
+    document.getElementById('valid-definition').innerHTML = ""
+
+    let word = document.getElementById(wordInput).value;
+    word = word.trim();
+    let definition = document.getElementById(wordDefinition).value;
+    definition = definition.trim();
+
+    let valid = validateInput(word);
+    if(!valid) {
+        document.getElementById('valid-word').innerHTML = "Invalid word"
+        return;
+    }
+
+    valid = validateInput(definition);
+    if(!valid) {
+        document.getElementById('valid-definition').innerHTML = "Invalid definintion"
+        return;
+    }
+
+    let wordObject = {word: word, definition: definition};
+
+    console.log(wordObject);
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "http://localhost:8888/", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(wordObject));
+
+    xhttp.onload = function () {
+        let response = xhttp.responseText;
+        console.log("response: " + response);
+        document.getElementById("success").innerHTML = response;
+    }
+
+}
+
+function get(input, output) {
+
+    document.getElementById('valid-word').innerHTML = "";
+    
+    let inputText = document.getElementById(input).value;
+
+    inputText = inputText.trim();
+    let valid = validateInput(inputText);
+    if(!valid) {
+        document.getElementById('valid-word').innerHTML = "Invalid word"
+        return;
+    }
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.responseText = "response";
+
+    xhttp.open("GET", "http://localhost:8888/?word="+inputText, true);
+
+    xhttp.send(inputText);
+
+    xhttp.onload = function() {
+        let response = xhttp.responseText;
+        document.getElementById(output).innerHTML = response;
+    }
+}
+
+let validateInput = (str) => {
+    let numbers = "0123456789";
+
+    if(str.length === 0) {
+        return false;
+    }
+    
+    if(str.trim().length === 0) {
+        return false;
+    }
+
+    for(let i = 0; i < str.length; i++) {
+        if(numbers.indexOf(str[i]) !== -1) {
+            return false;
+        }
+    }
+
+    return true;
+}
